29718792b38f6d4611fb988e7443d83e88a113fd
T8864 <ST10481162@rcconnect.edu.za>
2025-04-14 20:04:41 +0200

    Add files via upload

---
+++ ST10481162/test/qucikchat1/mainTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package qucikchat1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author loren
+ */
+public class mainTest {
+    
+    public mainTest() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+    
+}
